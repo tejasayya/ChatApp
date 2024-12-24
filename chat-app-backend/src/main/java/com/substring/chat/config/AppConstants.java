01249141d48e74f0f3daedb38d099fe06f3368c6
Sayya Teja Swaroop <63931564+tejasayya@users.noreply.github.com>
2024-12-24 17:50:57 -0500

    localhost+dkrimg

--- chat-app-backend/src/main/java/com/substring/chat/config/AppConstants.java
+++ chat-app-backend/src/main/java/com/substring/chat/config/AppConstants.java
@@ -2,4 +2,6 @@ package com.substring.chat.config;
 
 public class AppConstants {
     public static final String  FRONT_END_BASE_URL="http://localhost:5173";
+//    public static final String  FRONT_END_BASE_URL="http://64.227.156.141:5173";
+
 }
--- chat-front/src/config/AxiosHelper.js
+++ chat-front/src/config/AxiosHelper.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 export const baseURL = "http://localhost:8080";
+// export const baseURL = "http://64.227.156.141:8080";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
